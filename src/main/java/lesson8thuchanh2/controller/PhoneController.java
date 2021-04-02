bfe75e60302e3c7f2d689010b56bb8cf4656e484
Yumibk82 <ttthang0102@gmail.com>
2021-04-02 11:19:39 +0700

    thuc hanh 2

---
+++ src/main/java/lesson8thuchanh2/controller/PhoneController.java
@@ -0,0 +1,33 @@
+package lesson8thuchanh2.controller;
+
+import lesson8thuchanh2.model.PhoneNumber;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+//@RequestMapping("phone")
+public class PhoneController {
+    @GetMapping("")
+    public String showForm(Model model){
+        model.addAttribute("phonenumber",new PhoneNumber());
+        return "index";
+    }
+    @PostMapping("")
+    public String checkValidation(@Valid @ModelAttribute("phonenumber")PhoneNumber phoneNumber, BindingResult bindingResult,Model model){
+        new PhoneNumber().validate(phoneNumber,bindingResult);
+        if(bindingResult.hasFieldErrors()){
+            return "index";
+        }
+        else {
+            model.addAttribute("phonenumber",phoneNumber.getNumber());
+            return "result";
+        }
+    }
+}
